fc2f60402b7c986f8ce15221a01d77285df7090f
Pavel Tishkov <pavel.tishkov@flant.com>
2024-02-27 09:54:36 +0300

    [candi] add ru docs
    Signed-off-by: Pavel Tishkov <pavel.tishkov@flant.com>

--- candi/cloud-providers/dvp/terraform-modules/master-node/provider.tf
+++ candi/cloud-providers/dvp/terraform-modules/master-node/provider.tf
@@ -1,13 +1,3 @@
-terraform {
-  # required_version = ">= 0.13.4"
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-    }
-  }
-}
-
-
 provider "kubernetes" {
   config_path = "./config"
   #kubeconfig_base64 = var.cfg
